b9a65947cc2d9a64812a83cfd6c2e9cd72bb8f19
Gregory Mortelle <grmortel@mi-c6-p2.s19.be>
2023-04-07 16:06:40 +0200

    add

---
+++ Libft/ft_memcpy.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: grmortel <grmortel@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/07 15:27:41 by grmortel          #+#    #+#             */
+/*   Updated: 2023/04/07 15:46:23 by grmortel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memcpy(void	*dst, const void *src, size_t n)
+{
+	size_t			i;
+	unsigned char	*destination;
+	unsigned char	*source;
+
+	i = 0;
+	destination = (unsigned char *)dst;
+	source = (unsigned char *)src;
+	while (destination[i] && source[i] && n > 0)
+	{
+		destination[i] = source[i];
+		i++;
+	}
+	destination[i] = '\0';
+	return (destination);
+}
+
+int main()
+{
+	char source[100] = "test";
+	char destination[100] = "tets";
+
+	printf("%s\n", ft_memcpy(source, destination, 100));
+	//ft_memcpy(source, destination, 10);
+}
