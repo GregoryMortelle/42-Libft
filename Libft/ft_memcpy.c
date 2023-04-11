f760297783cb549a18b26b112ddbe23dfd5a52f5
Gregory Mortelle <grmortel@fu-r2-p6.s19.be>
2023-04-11 19:30:58 +0200

    add

--- Libft/ft_memcpy.c
+++ Libft/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: grmortel <grmortel@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/07 15:27:41 by grmortel          #+#    #+#             */
-/*   Updated: 2023/04/11 16:18:39 by grmortel         ###   ########.fr       */
+/*   Updated: 2023/04/11 19:30:11 by grmortel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ void	*ft_memcpy(void	*dst, const void *src, size_t n)
 	unsigned char	*destination;
 	unsigned char	*source;
 
+	if (!dst && !src)
+		return (0);
 	i = 0;
 	destination = (unsigned char *)dst;
 	source = (unsigned char *)src;
@@ -28,3 +30,14 @@ void	*ft_memcpy(void	*dst, const void *src, size_t n)
 	}
 	return (destination);
 }
+
+/* int main(void)
+ {
+ 	char *str1 = NULL;
+ 	char str2[400];
+
+ 	ft_memcpy(NULL, NULL, 1);
+ 	printf("ft_memcpy : %s\n", str2);
+ 	printf("memcpy    : %s\n", (char *)memcpy(str1, str2, 7));
+ }
+*/
--- Libft/ft_memmove.c
+++ Libft/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: grmortel <grmortel@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/10 12:34:28 by grmortel          #+#    #+#             */
-/*   Updated: 2023/04/11 16:46:06 by grmortel         ###   ########.fr       */
+/*   Updated: 2023/04/11 19:29:49 by grmortel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
 	size_t	i;
-	
+
 	i = 0;
 	if (!dst && !src)
 		return (NULL);
@@ -32,14 +32,3 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 				((char *)dst)[len] = ((char *)src)[len];
 	return ((char *)dst);
 }
-
- /*int main(void)
- {
- 	char *str1 = NULL;
- 	char str2[400];
-
- 	memmove(NULL, NULL, 1);
- 	printf("ft_memcpy : %s\n", str2);
- 	printf("memcpy    : %s\n", (char *)memcpy(str1, str2, 7));
- }
-*/
